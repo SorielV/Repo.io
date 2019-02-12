42996b63066973b9398d48ab1c763919c9d8593c
SorielV <xxsorielxx@gmail.com>
2019-02-12 19:21:16 +0000

    restore script

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,7 +1,29 @@
-if [ -z "$dump" ]
+DB_HOST='127.0.0.1'
+DB_PORT='6666'
+DB_USER='postgres'
+DB_PASSWORD='postgres'
+DB_DATABASE='repo'
+
+if [ -z "$DUMP" ]
 then
-   echo '$dump is required'
-else
-  echo "$dump"
-  exec psql postgresql://postgres:postgres@127.0.0.1:6666/repo < "$dump"
+  echo '$DUMP is required'
+  exit 0
 fi
+
+function database()
+{
+  echo "Database"
+  DOCKER_DB_ID=`docker-compose ps -q db`
+  if [ -z "${DOCKER_DB_ID}" ]
+  then
+    docker-compose up -d db
+    DBD=true
+    DOCKER_DB_ID=`docker-compose ps -q db`
+  else
+    DBD=false
+  fi
+}
+
+database
+
+docker exec -i "${DOCKER_DB_ID}" pg_restore -C --clean --no-acl --no-owner -d postgres://"${DB_USER}":"${DB_PASSWORD}"@"${DB_HOST}":"${DB_PORT}"/"${DB_DATABASE}" "${DUMP}"
